7b7609b245f59642205deab01e3fd1e912b8e1a9
Patrick Pepels <ppepels@rhinofly.nl>
2018-08-02 15:06:27 +0200

    metadata updated, specfile now validates rubocop tests

--- spec/classes/puppetmodule_spec.rb
+++ spec/classes/puppetmodule_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'puppetmodule' do
   let(:title) { 'puppetmodule' }
 
-  it { is_expected.to compile }   # this is the simplest test possible to make sure the Puppet code compiles
-  # it { is_expected.to compile.with_all_deps }  # same as above except it will test all the dependencies
+  it { is_expected.to compile }
+  it { is_expected.to compile.with_all_deps }
 end
