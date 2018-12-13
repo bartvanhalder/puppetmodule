725e2ae74f9f2308d80f255bd9887381d514e547
Patrick <patrick@kaliber.net>
2018-12-13 15:44:30 +0100

    basic tests for classes, need to expand them still

---
+++ spec/classes/config_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'puppetmodule::config', type: :class do
+  let :pre_condition do
+    'include puppetmodule'
+  end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'with all defaults' do
+        it { is_expected.to compile }
+      end
+    end
+  end
+end
